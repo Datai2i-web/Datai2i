a6d9808779e8df56d02a5409d11bd1111f08062f
SRINIVAS DADI <srivardhandadi@gmail.com>
2023-09-22 11:27:56 +0530

    swiper js

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask , render_template 
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+
+uri = "mongodb+srv://datai2i-admin:EGwhDoTMIFAHqwoX@datai2i.efqtgwz.mongodb.net/?retryWrites=true&w=majority"
+
+client = MongoClient(uri, server_api=ServerApi('1'))
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-
+    return render_template('index.html', data = db)
 
 
 if __name__ == "__main__":
